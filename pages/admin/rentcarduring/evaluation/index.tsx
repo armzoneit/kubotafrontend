8ee0a277a1b58d02678b955a68780aa5bc646d27
BallkungZa <contestball@hotmail.com>
2024-08-26 11:21:38 +0700

    xxxx

--- pages/admin/rentcarduring/evaluation/index.tsx
+++ pages/admin/rentcarduring/evaluation/index.tsx
@@ -42,16 +42,16 @@ const List =  () => {
     const me = getMe()
     const router = useRouter()
 
-    if(me.data){
-        if(me.data.data.permissionReserve){
-            let pass =  me.data.data.permissionReserve.find(x => x.mode == 3 && x.menu == 4)?.approved;
-            if(!pass){
-                router.push("/admin/users");
-            }
-        }else{
-            router.push("/admin/users");
-        }
-    }
+    // if(me.data){
+    //     if(me.data.data.permissionReserve){
+    //         let pass =  me.data.data.permissionReserve.find(x => x.mode == 3 && x.menu == 4)?.approved;
+    //         if(!pass){
+    //             router.push("/admin/users");
+    //         }
+    //     }else{
+    //         router.push("/admin/users");
+    //     }
+    // }
     return (
         <>
             <IndexPage mode={3} />
